dab52e988a9b60408dd60732afbcfaab8db497e7
Eva Royo <Eva Royo@DESKTOP-L5C371F>
2018-05-15 20:16:35 +0200

    Pongo patas

--- src/octopus/Octopus.java
+++ src/octopus/Octopus.java
@@ -16,6 +16,10 @@ public class Octopus {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("soy un pulpo");
+        for (int i = 0; i < 7; i++) {
+            System.out.println("pata");
+        }
     }
     
 }
