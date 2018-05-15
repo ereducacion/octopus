ee1e23e5bfa682ef06218be320c8169cdc4e23c5
Eva Royo <Eva Royo@DESKTOP-L5C371F>
2018-05-15 20:20:01 +0200

    Añado el gatopulpo

---
+++ src/octopus/Octopuscat.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package octopus;
+
+/**
+ *
+ * @author Eva Royo
+ */
+public class Octopuscat {
+    
+    private int numpatas;
+
+    /**
+     * @return the numpatas
+     */
+    public int getNumpatas() {
+        return numpatas;
+    }
+
+    /**
+     * @param numpatas the numpatas to set
+     */
+    public void setNumpatas(int numpatas) {
+        this.numpatas = numpatas;
+    }
+}
